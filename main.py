f9cc302268c39e089e89c173714513c2f421dfef
Moin Uddin <moin77796@gmail.com>
2024-07-24 19:49:10 +0100

    chore: Disable GPU in main.py for scraping Indeed reviews

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ def scrape_indeed_reviews(url, pages, email, password):
     options.add_argument("--start-maximized")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--disable-gpu')
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option("useAutomationExtension", False)
